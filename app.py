9f24870602a4bf290440781e6b2c8d02f36f957c
Debasish Sarangi <debasish.sarangi3@gmail.com>
2022-03-31 20:22:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,36 +17,7 @@ import flask
 app = Flask(__name__)
 clf = joblib.load('APIPredict.pkl')
 
-class MultiColumnLabelEncoder:
-
-    def __init__(self, columns=None):
-        self.columns = columns  # array of column names to encode
-
-    def fit(self, X, y=None):
-        self.encoders = {}
-        columns = X.columns if self.columns is None else self.columns
-        for col in columns:
-            self.encoders[col] = LabelEncoder().fit(X[col])
-        return self
-
-    def transform(self, X):
-        output = X.copy()
-        columns = X.columns if self.columns is None else self.columns
-        for col in columns:
-            output[col] = self.encoders[col].transform(X[col])
-        return output
-
-    def fit_transform(self, X, y=None):
-        return self.fit(X, y).transform(X)
-
-    def inverse_transform(self, X):
-        output = X.copy()
-        columns = X.columns if self.columns is None else self.columns
-        for col in columns:
-            output[col] = self.encoders[col].inverse_transform(X[col])
-        return output
-
-
+from train import MultiColumnLabelEncoder
 preprocessing_1 = joblib.load('Preprocessing_1.pkl')
 preprocessing_2 = joblib.load('Preprocessing_2.pkl')
 
