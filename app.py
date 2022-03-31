b39044ebe172ad1869c0d07750a063a7846e4f2c
Debasish Sarangi <debasish.sarangi3@gmail.com>
2022-03-31 18:52:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,32 +125,32 @@ if __name__ == '__main__':
     # count_vect = joblib.load('quora_vectorizer.pkl')
     class MultiColumnLabelEncoder:
 
-    def __init__(self, columns=None):
-        self.columns = columns  # array of column names to encode
+        def __init__(self, columns=None):
+            self.columns = columns  # array of column names to encode
 
-    def fit(self, X, y=None):
-        self.encoders = {}
-        columns = X.columns if self.columns is None else self.columns
-        for col in columns:
-            self.encoders[col] = LabelEncoder().fit(X[col])
-        return self
+        def fit(self, X, y=None):
+            self.encoders = {}
+            columns = X.columns if self.columns is None else self.columns
+            for col in columns:
+                self.encoders[col] = LabelEncoder().fit(X[col])
+            return self
 
-    def transform(self, X):
-        output = X.copy()
-        columns = X.columns if self.columns is None else self.columns
-        for col in columns:
-            output[col] = self.encoders[col].transform(X[col])
-        return output
+        def transform(self, X):
+            output = X.copy()
+            columns = X.columns if self.columns is None else self.columns
+            for col in columns:
+                output[col] = self.encoders[col].transform(X[col])
+            return output
 
-    def fit_transform(self, X, y=None):
-        return self.fit(X, y).transform(X)
+        def fit_transform(self, X, y=None):
+            return self.fit(X, y).transform(X)
 
-    def inverse_transform(self, X):
-        output = X.copy()
-        columns = X.columns if self.columns is None else self.columns
-        for col in columns:
-            output[col] = self.encoders[col].inverse_transform(X[col])
-        return output
+        def inverse_transform(self, X):
+            output = X.copy()
+            columns = X.columns if self.columns is None else self.columns
+            for col in columns:
+                output[col] = self.encoders[col].inverse_transform(X[col])
+            return output
 
 
     preprocessing_1 = joblib.load('Preprocessing_1.pkl')
